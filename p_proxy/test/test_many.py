1c8cffb3515bf65e4bbb78b8953215663e09c1b2
Gregory Kuhn <gregory.kuhn@analog.com>
2017-05-12 00:19:26 +0100

    Added a 'test_long_strings' test across multiple processes

--- p_proxy/test/test_many.py
+++ p_proxy/test/test_many.py
@@ -1,6 +1,8 @@
 import pytest
 from RefCls import RefCls
 from p_proxy.PProxy import PProxy
+import string
+import random
 
 def test_many():
     #lots!
@@ -9,5 +11,13 @@ def test_many():
         obj.print_a()
     #implicit shutdown expected
 
+def test_many_long_strings():
+    objs = [PProxy(RefCls, 1,2,3) for i in range(5)]
+    long_data = ''.join(random.choices(string.printable, k=200000))
+    for obj in objs:
+        for i in range(1000):
+            s_upper = obj.upper(long_data[:random.randint(100000,200000)],
+                        _async=True)
 
-
+    for obj in objs:
+        r = obj._pp_get_last()[0]
